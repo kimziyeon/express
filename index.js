38535532a917233b222d27f9e86eeb226dd325fd
kimziyeon <aa40254037@gmail.com>
2024-03-29 16:37:09 +0900

    portfolio add

--- index.js
+++ index.js
@@ -1,73 +1,23 @@
 const express = require('express')
-const fs = require('fs');
-var cors = require('cors');
-var bodyParser = require('body-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: '.env' })
 const app = express();
-let data = JSON.parse(fs.readFileSync('./db/data.json'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '10mb' }));
 
+const test = require('./router/test.js');
+const push = require('./router/push.js');
+const camera = require('./router/camera.js');
+const portfolio = require('./router/portfolio.js');
 
-app.use(cors()) //네트워크 문제를 해결하기위해
-
-
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
-
-
-app.get('/test', function (req, res) {
-    res.send(data.test);
-})
-
-
-app.get('/test/:id', function (req, res) {
-    const { id } = req.params;
-    const findData = data.test.find(obj => obj.id == id)
-    res.send(findData);
-})
+app.use('/', test);
+app.use('/push', push);
+app.use('/camera', camera);
+app.use('/portfolio', portfolio);
 
 
 
-app.post('/test', function (req, res) {
+app.listen(3000)
 
-    data.test.push(req.body);
-    const body = JSON.stringify(data);
-    const dataInsert = fs.writeFileSync('./db/data.json', body);
-    res.send(data.test);
-
-
-})
-
-
-app.delete('/test/:id', function (req, res) {
-
-    const { id } = req.params;
-    data.test = data.test.filter(obj => obj.id != id)
-
-    const body = JSON.stringify(data);
-    fs.writeFileSync('./db/data.json', body);
-
-    res.send(data.test);
-})
-
-
-app.put('/test/', function (req, res) {
-    const updateBody = req.body;
-
-    data.test = data.test.map(obj => {
-        if (obj.id == updateBody.id) {
-            obj = updateBody;
-        }
-        return obj;
-    })
-
-
-    const body = JSON.stringify(data);
-    fs.writeFileSync('./db/data.json', body);
-
-    res.send(data.test);
-})
-
-app.listen(3030)
-
-//http://localhost:3030/test/:id
---
+++ router/portfolio.js
@@ -0,0 +1,33 @@
+const testRouter = require('express').Router();
+const { MongoClient } = require('mongodb');
+
+const connectUrl = process.env.PORTFOLIO;
+const client = new MongoClient(connectUrl);
+let collection;
+
+const crud = async (type, info) => {
+
+    await client.connect();
+    const db = await client.db('portfolio');
+    collection = await db.collection('contact');
+
+    if (type === 'post') {
+        await collection.insertOne(info);
+    }
+    const data = await collection.find().toArray();
+
+    return data;
+}
+
+testRouter.get('/', async function (req, res) {
+    res.send(await crud('get'));
+})
+
+testRouter.post('/insert', async function (req, res) {
+    res.send(await crud('post', req.body));
+})
+
+
+module.exports = testRouter;
+
+
